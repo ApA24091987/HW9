65ceecf9840d322408f9b1ea76d2544cff4096c0
ApA24091987 <168029434+ApA24091987@users.noreply.github.com>
2024-05-16 22:25:09 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,15 @@
+def test_():
+    a = 10
+    b = 32
+    print('a =', a)
+    print('b =', b)
+
+test_()
+
+def test2(a, b, c,):
+    print(a, b, c)
+
+
+test2("Hello,", 'im', 'Yura')
+test2(a= 35, c= True, b= 'Liar' )
+
